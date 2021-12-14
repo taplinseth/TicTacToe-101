53eb566a21b09914a613f4bf79b83422da7d071b
taplinseth <taplinseth@gmail.com>
2021-12-14 14:43:40 -0600

    ttt logic

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""] 
+]
+
+
 //       ***********************
 //            INSTRUCTIONS
 //       ***********************
@@ -39,6 +46,10 @@ const handleClick = (element) => {
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
 
+  const row = parseInt(id.charAt(0))
+  const column = parseInt(id.charAt(2))
+  board[row][column] = currentMarker
+
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
   console.log(`*** The current marker is:  ${currentMarker}. ***`)
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
@@ -52,7 +63,7 @@ const addMarker = (id) => {
   // .innerHTML 
   document.getElementById(id).innerHTML = currentMarker
 
-  changeMarker()
+  checkForWin()
 }
 
 
@@ -75,15 +86,14 @@ const changeMarker = () => {
 
 
 
-
-
-
-
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
+
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""] 
+  ]
   
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
@@ -106,4 +116,54 @@ const resetBoard = () => {
     // sets the innerHTML to null to replace the "X" or "O"
     squares[i].innerHTML = null
   }  
+}
+
+
+const checkForWin = () => {
+  if(horizontalWin() || verticalWin() || diagonalWin()) {
+    window.alert(`Player ${currentMarker} won!`)
+  } else {
+    changeMarker()
+  }
+}
+
+
+//board diagram
+// 00 01 02
+// 10 11 12
+// 20 21 22
+
+
+const horizontalWin = () => {
+  if ((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")) {
+      return true
+  } else if ((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")) {
+      return true
+  } else if ((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) { 
+      return true
+  } else {
+      return false
+  }
+}
+
+const verticalWin = () => {
+  if ((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")) {
+      return true
+  } else if ((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")) {
+      return true 
+  } else if ((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")) {
+      return true
+  } else {
+      return false
+  }
+}
+
+const diagonalWin = () => {
+  if ((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")) {
+        return true
+    } else if ((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")) {
+        return true
+    } else {
+        return false
+    }
 }
